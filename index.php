34657b9d5c01d359bbde989e63bbc8c02a13bbd7
IwanByte <iwanbyte@gmail.com>
2020-10-23 21:53:39 +0700

    freepare projek

---
+++ index.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Home</title>
+  <!-- Bootstrap 4 -->
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+  <!-- navigation -->
+  <nav class="navbar navbar-expand bg-info navbar-dark">
+    <a href="#" class="navbar-brand">Manage Barang</a>
+  </nav>
+  <!-- end navogation -->
+
+  <br>
+
+  <!-- teks welcome -->
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <h1>Welcome My Store</h1>
+    </div>
+  </div>
+  <!-- end teks welcome -->
+
+  <br>
+
+   <!-- tombol ke halaman admin -->
+  <div class="row">
+    <div class="col-md-12 text-center">
+      <a href="tampil-barang.php" class="btn btn-primary">Admin Page</a>
+    </div>
+  </div>
+  <!-- end tombol -->
+
+</body>
+</html>
